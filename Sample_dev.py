0e8e558830ae1ac302dbd3aa89228344b753ac0d
PavanMathad <55686677+PavanMathad@users.noreply.github.com>
2020-01-08 12:52:29 +0530

    Update Sample_dev.py

--- Sample_dev.py
+++ Sample_dev.py
@@ -1,4 +1,9 @@
 #Sample code
-print("In Sample_dev.py In dev")
-print("to be merged to staging")
-return False
+def main():
+    print("In Sample_dev.py In dev")
+    print("to be merged to staging")
+    return True
+
+if __name__ == "__main__":
+    main()
+
