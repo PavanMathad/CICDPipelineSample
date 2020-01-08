66e106a4e452cd05dfe10e77a13471d4d48a5c2b
PavanMathad <55686677+PavanMathad@users.noreply.github.com>
2020-01-08 12:56:04 +0530

    Update automatic_code_review.py

--- automatic_code_review.py
+++ automatic_code_review.py
@@ -1,4 +1,8 @@
 #auto code review
+def main():
+    print("this script to review sample_code.py")
+    return False
 
-print("this script to review sample_code.py")
-return False
+if __name__ == "__main__":
+    main()
+
